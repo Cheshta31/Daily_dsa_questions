3479f668be94da312b639c2e5397bf7ae229105d
Cheshta31 <99382930+Cheshta31@users.noreply.github.com>
2023-08-05 22:19:53 +0530

    Create 2778. Sum of Squares of Special Elements.java

---
+++ 2778. Sum of Squares of Special Elements.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfSquares(int[] nums) {
+        int squaresum=0;int index=0;
+        for(int i=1;i<=nums.length;i++){
+            if((nums.length)%i==0){
+               index=i-1;
+                squaresum+=nums[index]*nums[index];
+                }
+            }
+        
+        return squaresum;
+    }
+}
