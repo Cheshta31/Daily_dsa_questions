d66663e4149ed06f73d262b8ff91222c5f0f3a4e
CHESHTA KHURANA <cheshtakhurana31@gmail.com>
2024-01-21 22:20:11 +0530

    Create Maximummoney.java

---
+++ Maximummoney.java
@@ -0,0 +1,17 @@
+//User function Template for Java
+class Solution{
+    public int MaxMoney(int M,int []B,int []A)
+    {
+        // code here
+        
+        int max=M;
+        for(int i=0;i<B.length;i++){
+            int buy=M/B[i];
+            int num=buy*B[i];
+            int rem=M-num;
+            int sell=(buy*A[i])+rem;
+            max=Math.max(max,sell);
+        }
+        return max;
+    }
+}
