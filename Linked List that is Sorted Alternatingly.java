a9ace7f0c9b8bc7c1b25fd0ae47d26fbb3484c70
CHESHTA KHURANA <cheshtakhurana31@gmail.com>
2024-03-15 09:44:55 +0530

    Create Linked List that is Sorted Alternatingly.java

---
+++ Linked List that is Sorted Alternatingly.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+   public Node sort(Node head){
+        //your code here, return the head of the sorted list
+        Node curr=head;
+        Node pre=null;
+        
+        LinkedList<Integer>al=new LinkedList<>();
+        while(curr!=null){
+            al.add(curr.data);
+            curr=curr.next;
+        }
+        curr=head;
+        Collections.sort(al);
+        for(int i=0;i<al.size();i++){
+            curr.data=al.get(i);
+            curr=curr.next;
+        }
+        
+        return head;
+        
+   }
+
+
+}
